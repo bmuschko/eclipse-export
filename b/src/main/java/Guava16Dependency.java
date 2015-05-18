af6dfd8c2eb4cb717b96fa9525e7bbf5972a2819
Benjamin Muschko <benjamin.muschko@gmail.com>
2015-05-18 09:45:26 -0400

    Added a shared class and library to project a to better demonstrate reuse in project b.

--- b/src/main/java/Guava16Dependency.java
+++ b/src/main/java/Guava16Dependency.java
@@ -5,6 +5,7 @@ import com.google.common.base.Enums;
 
 public class Guava16Dependency {
 	public static void main(String[] args) {
+		new Shared().doSomething();
 		// the converter class is introduced guava 16
 		Converter<String, AccessMode> converter = Enums.stringConverter(AccessMode.class);
 	}
